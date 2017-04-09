fb76e66c79b67734fa656cecb768dc7f5a718310
Nerond <nerond32@gmail.com>
2017-04-09 18:56:32 +0200

    client-server

--- PS1_l2/Program.cs
+++ PS1_l2/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace PS1_l2
 {
@@ -10,6 +10,108 @@ namespace PS1_l2
     {
         static void Main(string[] args)
         {
+            Socket s = null;
+            try
+            {
+                s = InitConnectionSocket();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Failed to connect to a server");
+            }
+            if (s != null)
+            {
+                WorkWithServer(s);
+                s.Close();
+            }
+            Console.ReadLine();
         }
+        
+        public static Socket InitConnectionSocket()
+        {
+            Console.WriteLine("Enter server address: ");
+            String sAddress = Console.ReadLine();
+            Console.WriteLine("Enter port: ");
+            int sPort = Convert.ToInt32(Console.ReadLine());
+            IPAddress ipAddress = null;
+            foreach (IPAddress tmpIPAddress in Dns.GetHostEntry(sAddress).AddressList)
+            {
+                if (tmpIPAddress.AddressFamily == AddressFamily.InterNetwork) // filter out ipv4
+                {
+                    ipAddress = tmpIPAddress;
+                }
+            }
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            s.Connect(ipAddress, sPort);
+            Console.WriteLine("Connection succesful");
+            return s;
+        }
+
+        public static void WorkWithServer(Socket s)
+        {
+            bool condition = false;
+            Console.WriteLine("Enter message to server: ");
+            String ts = Console.ReadLine();
+            do
+            {
+                byte[] toServer = Encoding.UTF8.GetBytes(ts);
+                s.Send(toServer);
+                Console.WriteLine("Message sent to server: " + ts);
+                byte[] fromServer = new byte[s.ReceiveBufferSize];
+                int length = s.Receive(fromServer);
+                String fs = Encoding.UTF8.GetString(fromServer);
+                if (!String.IsNullOrEmpty(fs))
+                {
+                    condition = !condition;
+                    Console.WriteLine("Message received from server: " + fs.Substring(0, length));
+                }
+            } while (!condition);
+        }
+        /*public static TcpClient InitConnection()
+        {
+            Console.WriteLine("Enter server address: ");
+            String sAddress = Console.ReadLine();
+            Console.WriteLine("Enter port: ");
+            int sPort = Convert.ToInt32(Console.ReadLine());
+            IPAddress ipAddress = null;
+            foreach (IPAddress tmpIPAddress in Dns.GetHostEntry(sAddress).AddressList)
+            {
+                if (tmpIPAddress.AddressFamily == AddressFamily.InterNetwork) // filter out ipv4
+                {
+                    ipAddress = tmpIPAddress;
+                }
+            }
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, sPort);
+            TcpClient tcpClient = new TcpClient();
+            tcpClient.Connect(ipEndPoint);
+            Console.WriteLine("Connection succesful");
+            return tcpClient;
+        }*/
+        /*public static void WorkWithServer(TcpClient tcpClient)
+        {
+            bool condition = false;
+            String ts = Console.ReadLine();
+            NetworkStream netStream = tcpClient.GetStream();
+            do
+            {
+                if (netStream.CanWrite)
+                {
+                    byte[] toServer = Encoding.UTF8.GetBytes(ts);
+                    netStream.Write(toServer, 0, toServer.Length);
+                    Console.WriteLine("Message sent to server: " + ts);
+                }
+                if (netStream.CanRead)
+                {
+                    byte[] fromServer = new byte[tcpClient.ReceiveBufferSize];
+                    netStream.Read(fromServer, 0, (int)tcpClient.ReceiveBufferSize);
+                    String fs = Encoding.UTF8.GetString(fromServer);
+                    if (!String.IsNullOrEmpty(fs))
+                    {
+                        condition = !condition;
+                        Console.WriteLine("Message received from server: " + fs);
+                    }
+                }
+            } while (!condition);
+        }*/
     }
 }
---
+++ PS1_l3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+public class serv
+{
+    public static Socket InitServer()
+    {
+        IPAddress ipAd = IPAddress.Parse("127.0.0.1");
+        Console.WriteLine("Enter server port(default 7): ");
+        int sPort = 7;
+        try
+        {
+            sPort = Convert.ToInt32(Console.ReadLine());
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Using default port value");
+        }
+        Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        IPEndPoint localEndPoint = new IPEndPoint(ipAd, sPort);
+        s.Bind(localEndPoint);
+        return s;
+    }
+    public static void Main()
+    {
+        Socket serverSocket = InitServer();
+        Console.WriteLine("Starting server at: " + serverSocket.LocalEndPoint);
+        serverSocket.Listen(10);
+        while (true)
+        {
+            Console.WriteLine("Waiting for new connection.");
+            Socket clientSocket = serverSocket.Accept();
+            Console.WriteLine("Connection accepted from " + clientSocket.RemoteEndPoint.ToString());
+            byte[] fromServer = new byte[clientSocket.ReceiveBufferSize];
+            int length = clientSocket.Receive(fromServer);
+            String ts = Encoding.UTF8.GetString(fromServer).Substring(0, length);
+            Console.WriteLine("Message received from client: " + ts);
+            clientSocket.Send(Encoding.UTF8.GetBytes(ts));
+            clientSocket.Close();
+        }
+        serverSocket.Close();
+        Console.ReadLine();
+    }
+
+}
